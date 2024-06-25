6257d315bc51782ca680a759cd84a0b5d369f91c
ManuelaNani <dasilvananimanuela@gmail.com>
2024-06-25 12:40:18 -0300

    adicionando arquivos css e js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+createCanvas(600, 600);
+background("white");
+}
+
+function draw() {
+
+sfunction setup() {
+troke("blue");
+fill("red");
+
+// console.log(mouseIsPressed)
+
+if (mouseIsPressed) {
+  rect(mouseX, mouseY, 20, 35);
+}
+}
